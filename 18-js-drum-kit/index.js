badf955a89386a14933b301d9fcf260e3f209f3b
artymiz <artyang.ca@gmail.com>
2024-01-27 23:02:26 -0800

    18: construct new audio whenever button is pressed for more speed

--- 18-js-drum-kit/index.js
+++ 18-js-drum-kit/index.js
@@ -1,13 +1,13 @@
 // index.js
 
 // setup audio files for different drum sounds
-const crash = new Audio("sounds/crash.mp3");
-const kick = new Audio("sounds/kick-bass.mp3");
-const snare = new Audio("sounds/snare.mp3");
-const tom1 = new Audio("sounds/tom-1.mp3");
-const tom2 = new Audio("sounds/tom-2.mp3");
-const tom3 = new Audio("sounds/tom-3.mp3");
-const tom4 = new Audio("sounds/tom-4.mp3");
+const crash = "sounds/crash.mp3";
+const kick = "sounds/kick-bass.mp3";
+const snare = "sounds/snare.mp3";
+const tom1 = "sounds/tom-1.mp3";
+const tom2 = "sounds/tom-2.mp3";
+const tom3 = "sounds/tom-3.mp3";
+const tom4 = "sounds/tom-4.mp3";
 
 // setup event listeners for each drum button
 const buttonAudioMapping = {
@@ -32,7 +32,8 @@ function playButtonAnimation(buttonText) {
 document.querySelectorAll(`[class*="drum"]`).forEach((button) => {
   button.addEventListener("click", function () {
     const buttonText = this.innerHTML;
-    buttonAudioMapping[buttonText].play();
+    // buttonAudioMapping[buttonText].play();
+    new Audio(buttonAudioMapping[buttonText]).play();
     playButtonAnimation(buttonText);
   });
 });
@@ -40,7 +41,8 @@ document.querySelectorAll(`[class*="drum"]`).forEach((button) => {
 Object.entries(buttonAudioMapping).forEach(([key, value]) => {
   addEventListener("keydown", (e) => {
     if (e.key === key) {
-      value.play();
+      // value.play();
+      new Audio(value).play();
       playButtonAnimation(key);
     }
   });
