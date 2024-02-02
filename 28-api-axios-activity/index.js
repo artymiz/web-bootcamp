38035843e2dbee0b3775ebe70b31a011b487b123
artymiz <artyang.ca@gmail.com>
2024-02-02 01:19:41 -0800

    api-axios: completed

--- 28-api-axios-activity/index.js
+++ 28-api-axios-activity/index.js
@@ -1,17 +1,16 @@
 import express from "express";
-import bodyParser from "body-parser";
 import axios from "axios";
 
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 // Step 1: Make sure that when a user visits the home page,
 //   it shows a random activity.You will need to check the format of the
 //   JSON data from response.data and edit the index.ejs file accordingly.
-app.get("/", async (req, res) => {
+app.get("/", async (_, res) => {
   try {
     const response = await axios.get("https://bored-api.appbrewery.com/random");
     const result = response.data;
@@ -35,8 +34,35 @@ app.post("/", async (req, res) => {
   // Step 3: If you get a 404 error (resource not found) from the API request.
   // Pass an error to the index.ejs to tell the user:
   // "No activities that match your criteria."
+  try {
+    const type = req.body.type;
+    const participants = req.body.participants;
+    const response = await axios.get(
+      `https://bored-api.appbrewery.com/filter?type=${type}&participants=${participants}`,
+    );
+    const result = response.data;
+    res.render("index.ejs", {
+      data: result[Math.floor(Math.random() * result.length)]
+    });
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
+      error: getErrorMessage(error.response.status),
+    });
+  }
 });
 
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`);
 });
+
+const getErrorMessage = (statusCode) => {
+  switch (statusCode) {
+    case 404:
+      return "No activities that match your criteria.";
+    case 429:
+      return "Too many resquests too soon. Please wait and try again later.";
+    default:
+      return "Something unexpected happened. Please try again later.";
+  }
+};
