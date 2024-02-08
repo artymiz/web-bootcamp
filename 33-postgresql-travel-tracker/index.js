33ffec1e00d289d0bfe99ab93aafd8dc81c79793
artymiz <artyang.ca@gmail.com>
2024-02-08 14:54:14 -0800

    pgs travel tracker: start

---
+++ 33-postgresql-travel-tracker/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", async (req, res) => {
+  //Write your code here.
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
