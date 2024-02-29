4262bf6594109b378492a17abef51f4dc4312f9c
artymiz <artyang.ca@gmail.com>
2024-02-29 00:57:24 -0800

    react-keeper-app: complete add/remove items

---
+++ 36-react-keeper-app/src/components/CreateItemBox.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+
+export default function CreateItemBox({ onAdd }) {
+  let [note, setNote] = useState({ title: '', content: '' });
+
+  function handleChange(event) {
+    let { name, value } = event.target;
+    setNote(prevValue => {
+      return {
+        ...prevValue,
+        [name]: value.trim()
+      };
+    });
+  }
+
+  function submit(event) {
+    event.preventDefault();
+    if (note.title === '' || note.content === '') {
+      return;
+    }
+    onAdd(note);
+    setNote({ title: '', content: '' });
+  }
+
+  return (
+    <div>
+      <form>
+        <input
+          name="title"
+          placeholder="Title"
+          onChange={handleChange}
+          value={note.title}
+        />
+        <textarea
+          name="content"
+          placeholder="Take a note..."
+          onChange={handleChange}
+          value={note.content}
+        />
+        <button onClick={submit}>Add</button>
+      </form>
+    </div>
+  );
+}
+
