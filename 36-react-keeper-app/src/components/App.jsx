99a1b142024bf67acbb56e14ba4abc19fa417b8a
artymiz <artyang.ca@gmail.com>
2024-02-29 00:58:21 -0800

    react-keeper-app: complete add/remove items

--- 36-react-keeper-app/src/components/App.jsx
+++ 36-react-keeper-app/src/components/App.jsx
@@ -1,20 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './Header';
 import Note from './Note';
+import CreateItemBox from './CreateItemBox';
 import Footer from './Footer';
-import notes from '../data/notes';
 
 export default function App() {
+  let [notes, setNotes] = useState([]);
+
+  function createNote(newNote) {
+    setNotes(prevValue => {
+      return [...prevValue, newNote];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes(prevValue => {
+      return prevValue.filter((_, index) => { return index !== id; });
+    });
+  }
+
   return (
     <div>
       <Header />
+      <CreateItemBox onAdd={createNote} />
       {
-        notes.map(note => {
-          return <Note
-            key={note.key}
-            title={note.title}
-            content={note.content}
-          />;
+        notes.map((note, index) => {
+          return (
+            <Note
+              key={index}
+              id={index}
+              title={note.title}
+              content={note.content}
+              onDelete={deleteNote}
+            />
+          );
         })
       }
       <Footer />
--- 36-react-keeper-app/src/components/Note.jsx
+++ 36-react-keeper-app/src/components/Note.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
-export default function Note({title, content}) {
+export default function Note({ id, title, content, onDelete }) {
+  function handleClick() {
+    onDelete(id);
+  }
+
   return (
     <div className="note">
       <h1>{title}</h1>
       <p>{content}</p>
+      <button onClick={handleClick}>DELETE</button>
     </div>
   );
 }
